d071c78cbdcfba573323e29ab3c110f1c9a346eb
JooeonLee <58818779+JooeonLee@users.noreply.github.com>
2023-07-06 12:28:48 +0900

    [Silver III] Title: N과 M (1), Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int n, m;
+int arr[10];
+int chk[10];
+
+void dfs(int cnt){
+    if(cnt == m){
+        for(int i = 0; i < m; i++)
+            cout << arr[i] << ' ';
+        cout << '\n';
+        return;
+    }
+    for(int i = 1; i <= n; i++){
+        if(!chk[i]){
+            chk[i] = 1;
+            arr[cnt] = i;
+            dfs(cnt+1);
+            chk[i] = 0;
+        }
+    }
+}
+
+int main() {
+	ios_base :: sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+    cin >> n >> m;
+    dfs(0);
+}
