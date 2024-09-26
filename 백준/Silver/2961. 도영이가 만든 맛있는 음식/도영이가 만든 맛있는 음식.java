6513c8581ced1000735b453b76354b8038e6d06f
JooeonLee <58818779+JooeonLee@users.noreply.github.com>
2024-09-26 20:08:18 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 100 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -10,15 +10,10 @@ public class Main {
     static long diff = Long.MAX_VALUE;
     static int[] sourness_arr = new int[10];
     static int[] bitterness_arr = new int[10];
-    static boolean[] isSelected = new boolean[10];
 
     public static void main(String[] args) {
         input();
-        for(int r=1; r<N+1; r++) {
-            init_isSelected();
-            combination(0, 0, r);
-        }
-
+        minDiff();
         System.out.println(diff);
     }
 
@@ -31,45 +26,22 @@ public class Main {
         }
     }
 
-
-    static void combination(int start, int selected, int r) {
-        if(selected == r) {
-            calculate();
-            return;
-        }
-
-        for(int i = start; i < N; i++) {
-            isSelected[i] = true;
-            combination(i+1, selected+1, r);
-            isSelected[i] = false;
-        }
-    }
-
-    static void calculate() {
-        long cur_s_mul = 1;
-        long cur_b_sum = 0;
-        int count = 0;
-
-        for(int i = 0; i<N; i++) {
-            if(isSelected[i]) {
-                cur_s_mul *= sourness_arr[i];
-                cur_b_sum += bitterness_arr[i];
-                count++;
+    static void minDiff() {
+        int lth = 1 << N;
+        for(int i=1; i<lth; i++) {
+            int current_s = 1;
+            int current_b = 0;
+            for(int j=0; j<N; j++) {
+                if((i & 1<<j) > 0) { // i의 j 번째 비트가 1인 경우
+                    current_s *= sourness_arr[j];
+                    current_b += bitterness_arr[j];
+                }
             }
-        }
-
-        if(count>0) {
-            long cur_diff = Math.abs(cur_s_mul - cur_b_sum);
-            if(cur_diff < diff) {
-                diff = cur_diff;
-            }
+            int current_diff = Math.abs(current_s - current_b);
+            diff = Math.min(diff, current_diff);
         }
     }
 
-    static void init_isSelected() {
-        for(int i=0; i<10; i++)
-            isSelected[i] = false;
-    }
     static class FastReader {
         BufferedReader br;
         StringTokenizer st;
