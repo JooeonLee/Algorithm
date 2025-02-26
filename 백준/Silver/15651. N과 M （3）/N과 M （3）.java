af4e748895fd7f7b80268a49aaf992057d4e9fd8
JooeonLee <58818779+JooeonLee@users.noreply.github.com>
2025-02-26 23:59:58 +0900

    [Silver III] Title: N과 M (3), Time: 368 ms, Memory: 68560 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,85 +1,70 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
 
-    static void input() {
-        FastReader scan = new FastReader();
-        N = scan.nextInt();
-        M = scan.nextInt();
-        selected = new int[M + 1];
-    }
-
-    static int N, M;
-    static int[] selected;
-
-    static void rec_func(int k) {
-        if (k == M + 1) { // 다 골랐다!
-            // selected[1...M] 배열이 새롭게 탐색된 결과
-            for (int i = 1; i <= M; i++) sb.append(selected[i]).append(' ');
-            sb.append('\n');
-        } else {
-            for (int cand = 1; cand <= N; cand++) {
-                // k 번째에 cand 가 올 수 있으면
-                selected[k] = cand;
-
-                // k+1 번부터 M 번까지 잘 채워주는 함수를 호출해준다.
-                rec_func(k + 1);
-                selected[k] = 0;
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        input();
-        rec_func(1);
-        System.out.println(sb.toString());
-    }
-
-    static class FastReader {
-        BufferedReader br;
-        StringTokenizer st;
-
-        public FastReader() {
-            br = new BufferedReader(new InputStreamReader(System.in));
-        }
-
-        public FastReader(String s) throws FileNotFoundException {
-            br = new BufferedReader(new FileReader(new File(s)));
-        }
-
-        String next() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
-
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
-        String nextLine() {
-            String str = "";
-            try {
-                str = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
-        }
-    }
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+		
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		
+		String next() {
+			while(st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch(IOException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			return st.nextToken();
+		}
+		
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+	
+	// 전역 변수들
+	static StringBuilder sb = new StringBuilder();
+	static int N, M;
+	static int[] selected;
+	
+	// 입력함수
+	static void input() {
+		FastReader scan = new FastReader();
+		N = scan.nextInt();
+		M = scan.nextInt();
+		selected = new int[M+1];
+	}
+	
+	// PS 함수
+	// 재귀 함수
+	static void rec_func(int k) {
+		if(k == M+1) {
+			for(int i=1; i<=M; i++)
+				sb.append(selected[i]).append(' ');
+				sb.append('\n');
+		} else {
+			for(int cand=1; cand<=N; cand++) {
+				selected[k] = cand;
+				rec_func(k+1);
+				selected[k] = 0;
+			}
+		}
+	}
+	
+	// main 함수
+	public static void main(String[] args) {
+		input();
+		rec_func(1);
+		System.out.println(sb.toString());
+	}
 }
