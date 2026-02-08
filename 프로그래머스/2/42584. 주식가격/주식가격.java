7e02c965ed1bc68f6548232b06ca5867eab2c8ab
JooeonLee <58818779+JooeonLee@users.noreply.github.com>
2026-02-08 20:22:00 +0900

    [level 2] Title: 주식가격, Time: 43.27 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int n = prices.length;
+        int[] answer = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            // 가격이 떨어지는 순간
+            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                int idx = stack.pop();
+                answer[idx] = i - idx;
+            }
+            stack.push(i);
+        }
+
+        // 끝까지 가격이 떨어지지 않은 경우
+        while (!stack.isEmpty()) {
+            int idx = stack.pop();
+            answer[idx] = n - 1 - idx;
+        }
+
+        return answer;
+    }
+}
