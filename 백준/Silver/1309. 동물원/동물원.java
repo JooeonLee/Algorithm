b4ea4d58c03e2132f4138f52b97817f992624381
JooeonLee <58818779+JooeonLee@users.noreply.github.com>
2026-03-29 22:54:57 +0900

    [Silver I] Title: 동물원, Time: 120 ms, Memory: 17740 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.java
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -1,46 +1,30 @@
 import java.io.*;
 import java.util.*;
-import java.lang.Math;
 
 public class Main {
-    static FastReader scan = new FastReader();
-    static StringBuilder sb = new StringBuilder();
 
-    static int N;
     static final int MOD = 9901;
-    static int[][] Dy;
-    
-    static void input(){
-        N = scan.nextInt();
-        Dy = new int[N + 1][3];
-    }
-
-    static void pro() {
-        // 초기값 구하기
-        Dy[1][0] = 1;
-        Dy[1][1] = 1;
-        Dy[1][2] = 1;
-    
-        // 점화식을 토대로 Dy 배열 채우기
-        for (int i = 2; i <= N; i++) {
-            for (int prev = 0; prev <= 2; prev++) {
-                Dy[i][0] += Dy[i - 1][prev];
-                Dy[i][0] %= MOD;
-                for (int j = 0; j <= 1; j++) {
-                    if ((prev & (1 << j)) != 0) continue;
-                    Dy[i][1 << j] += Dy[i - 1][prev];
-                    Dy[i][1 << j] %= MOD;
-                }
-            }
-        }
-        System.out.println((Dy[N][0] + Dy[N][1] + Dy[N][2]) % MOD);
-    }
 
     public static void main(String[] args) {
-        input();
-        pro();
-    }
+        FastReader fr = new FastReader();
 
+        int N = fr.nextInt();
+
+        int[][] dp = new int[N + 1][3];
+
+        dp[1][0] = 1; 
+        dp[1][1] = 1; 
+        dp[1][2] = 1; 
+
+        for (int i = 2; i <= N; i++) {
+            dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % MOD;
+            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % MOD;
+            dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % MOD;
+        }
+
+        int result = (dp[N][0] + dp[N][1] + dp[N][2]) % MOD;
+        System.out.println(result);
+    }
 
     static class FastReader {
         BufferedReader br;
@@ -50,16 +34,12 @@ public class Main {
             br = new BufferedReader(new InputStreamReader(System.in));
         }
 
-        public FastReader(String s) throws FileNotFoundException {
-            br = new BufferedReader(new FileReader(new File(s)));
-        }
-
         String next() {
-            while (st == null || !st.hasMoreElements()) {
+            while (st == null || !st.hasMoreTokens()) {
                 try {
                     st = new StringTokenizer(br.readLine());
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    throw new RuntimeException(e);
                 }
             }
             return st.nextToken();
@@ -67,24 +47,6 @@ public class Main {
 
         int nextInt() {
             return Integer.parseInt(next());
-        }
-
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
-        String nextLine() {
-            String str = "";
-            try {
-                str = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
         }
     }
 }
