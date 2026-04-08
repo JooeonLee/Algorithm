a8c78be42abf809b71beb292eef2b071142570cd
JooeonLee <58818779+JooeonLee@users.noreply.github.com>
2026-04-08 13:46:26 +0900

    [level 2] Title: 기능개발, Time: 8.54 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,36 +2,32 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> daysList = new ArrayList<>();
+        ArrayList<Integer> daysList = new ArrayList<>();
         
         for(int i=0; i<progresses.length; i++) {
-            int remainingProgress = 100 - progresses[i];
-            int days = (int)Math.ceil((double)remainingProgress / speeds[i]);
-            daysList.add(days);
+            int remain = 100 - progresses[i];
+            if(remain%speeds[i] == 0)
+                daysList.add(remain/speeds[i]);
+            else
+                daysList.add(remain/speeds[i] + 1);
         }
         
-        List<Integer> answerList = new ArrayList<>();
-        
-        int distributeDay = daysList.get(0);
+        ArrayList<Integer> answerList = new ArrayList<>();
         int count = 1;
+        int daysD = daysList.get(0);
         
         for(int i=1; i<daysList.size(); i++) {
-            if(daysList.get(i) <= distributeDay) {
+            int current = daysList.get(i);
+            if(daysD >= current)
                 count++;
-            } else {
+            else {
                 answerList.add(count);
-                distributeDay = daysList.get(i);
                 count = 1;
+                daysD = current;
             }
         }
-        
         answerList.add(count);
         
-        int[] answer = new int[answerList.size()];
-        for(int i=0; i<answerList.size(); i++) {
-            answer[i] = answerList.get(i);
-        }
-        
-        return answer;
+        return answerList.stream().mapToInt(i -> i).toArray();
     }
 }
