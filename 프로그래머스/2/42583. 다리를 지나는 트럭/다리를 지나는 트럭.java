9877ebabe50cc1a9081ca5746c10380d916c5fc0
JooeonLee <58818779+JooeonLee@users.noreply.github.com>
2026-04-08 19:27:29 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 29.90 ms, Memory: 94.9 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,36 +2,32 @@ import java.util.*;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        Queue<Integer> bridge = new LinkedList<>();
+        Deque<Integer> deque = new LinkedList<>();
+        for(int i=0; i<bridge_length; i++)
+            deque.offer(0);
         
-        // 다리를 처음엔 빈 상태로 채움
-        for (int i = 0; i < bridge_length; i++) {
-            bridge.offer(0);
-        }
-
+        int idx = 0;
         int time = 0;
-        int currentWeight = 0;
-        int truckIndex = 0;
-
-        while (truckIndex < truck_weights.length) {
+        int remainWeight = weight;
+        while(!deque.isEmpty() && idx < truck_weights.length) {
             time++;
-
-            // 1. 다리에서 한 칸 이동 (트럭 내려감)
-            currentWeight -= bridge.poll();
-
-            // 2. 다음 트럭을 올릴 수 있는지 확인
-            if (currentWeight + truck_weights[truckIndex] <= weight) {
-                bridge.offer(truck_weights[truckIndex]);
-                currentWeight += truck_weights[truckIndex];
-                truckIndex++;
-            } else {
-                bridge.offer(0); // 못 올라가면 빈 공간
+            int outWeight = deque.pollLast();
+            remainWeight += outWeight;
+            
+            if(remainWeight >= truck_weights[idx]) {
+                deque.offerFirst(truck_weights[idx]);
+                remainWeight -= truck_weights[idx];
+                idx++;
+            }
+            else {
+                deque.offerFirst(0);
             }
         }
-
-        // 마지막 트럭이 다리를 완전히 건너는 시간
-        time += bridge_length;
-
+        
+        while(!deque.isEmpty()) {
+            time++;
+            deque.pollLast();
+        }
         return time;
     }
 }
