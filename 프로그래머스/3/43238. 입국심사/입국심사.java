5199b8d6f0eec6b514c86f609553d28f8b4ec1fd
JooeonLee <58818779+JooeonLee@users.noreply.github.com>
2026-04-22 18:55:54 +0900

    [level 3] Title: 입국심사, Time: 49.61 ms, Memory: 99.5 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,36 +1,35 @@
-import java.util.*;
-
 class Solution {
     public long solution(int n, int[] times) {
-        
-        
-        return search(times, n);
+        return bSearch(n, times);
     }
     
-    public long search(int[] times, int n) {
-        Arrays.sort(times);
+    public static boolean determine(int n, long currTime, int[] times) {
+        long cnt = 0;
+        for(int time : times)
+            cnt += currTime / time;
         
+        if(cnt >= n)
+            return true;
+        else
+            return false;
+    }
+    
+    public static long bSearch(int n, int[] times) {
+        long mid = 0;
         long left = 0;
-        long right = (long) times[times.length-1] * n;
-        long answer = 0;
+        long right = 1000000000000000000L;
         
-        while(left <= right) {
-            long mid = (left + right) / 2;
+        while(left < right) {
+            mid = (left + right) / 2;
             
-            long count = 0;
-            for(int time : times) {
-                count += mid/time;
+            if(determine(n, mid, times)) {
+                right = mid;
             }
-            
-            if(count < n) {
+            else {
                 left = mid + 1;
-            } else {
-                answer = mid;
-                right = mid - 1;
             }
         }
         
-        return answer;
+        return left;
     }
-    
 }
