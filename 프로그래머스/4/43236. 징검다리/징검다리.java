4034db9a9c9f7e8f05a076adef2841bf11038513
JooeonLee <58818779+JooeonLee@users.noreply.github.com>
2026-04-22 20:30:32 +0900

    [level 4] Title: 징검다리, Time: 38.31 ms, Memory: 93.1 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.java
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -3,40 +3,45 @@ import java.util.*;
 class Solution {
     public int solution(int distance, int[] rocks, int n) {
         Arrays.sort(rocks);
-
-        int left = 1;          // 가능한 최소 거리(하한)
-        int right = distance;  // 가능한 최소 거리(상한)
-        int answer = 0;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2; // "최소 거리"를 mid 이상으로 만들 수 있나?
-            if (canKeepMinDistance(distance, rocks, n, mid)) {
-                answer = mid;      // 가능하면 더 키워본다
-                left = mid + 1;
-            } else {
-                right = mid - 1;   // 불가능하면 줄인다
-            }
-        }
-        return answer;
+        return ps(distance, rocks, n);
     }
-
-    // minDist를 만족하려면 제거해야 하는 바위 개수가 n 이하인지 체크
-    private boolean canKeepMinDistance(int distance, int[] rocks, int n, int minDist) {
-        int removed = 0;
-        int prev = 0; // 출발점(0)
-
-        for (int r : rocks) {
-            if (r - prev < minDist) {
-                removed++; // 너무 가까우니 현재 바위를 제거
-                if (removed > n) return false;
-            } else {
-                prev = r; // 유지
+    
+    public static boolean determine(int dist, int[] sortedRocks, int endPos, int n) {
+        int cnt=0;
+        
+        int postPos = 0;
+        for(int i=0; i<sortedRocks.length; i++) {
+            int currDist = sortedRocks[i] - postPos;
+            if(currDist < dist) {
+                cnt++;
             }
+            else
+                postPos = sortedRocks[i];
         }
-
-        // 마지막(도착점) 간격 체크
-        if (distance - prev < minDist) removed++;
-
-        return removed <= n;
+        
+        if(endPos - postPos < dist)
+            cnt++;
+        
+        if(cnt <= n)
+            return true;
+        else
+            return false;
+    }
+    
+    public static int ps(int distance, int[] sortedRocks, int n) {
+        int left = 0;
+        int right = distance;
+        
+        while(left < right) {
+            int mid = left + (right - left + 1)/2;
+            
+            if(determine(mid, sortedRocks, distance, n)) {
+                left = mid;
+            }
+            else
+                right = mid-1;
+        }
+        
+        return left;
     }
 }
