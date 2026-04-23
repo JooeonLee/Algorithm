aa0092254978a8a307ab4defc93a0ae5f3f43688
JooeonLee <58818779+JooeonLee@users.noreply.github.com>
2026-04-23 20:29:05 +0900

    [level 3] Title: 네트워크, Time: 0.26 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 87.3 MB, 시간: 0.27 ms
+메모리: 78.1 MB, 시간: 0.26 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 28일 23:33:55
+2026년 04월 23일 20:29:02
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,34 +1,28 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n, int[][] computers) {
-        boolean[] visited = new boolean[n];
-        int networks = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                networks++;
-                bfs(i, n, computers, visited);
+        int[] visited = new int[n];
+        int answer = 0;
+        
+        for(int i=0; i<n; i++) {
+            if(visited[i] == 0) {
+                answer++;
+                dfs(i, n, computers, visited);
             }
         }
-
-        return networks;
+            
+        return answer;
     }
     
-    private void bfs(int start, int n, int[][] computers, boolean[] visited) {
-        Queue<Integer> q = new ArrayDeque<>();
-        q.offer(start);
-        visited[start] = true;
-
-        while (!q.isEmpty()) {
-            int cur = q.poll();
-
-            for (int next = 0; next < n; next++) {
-                if (!visited[next] && computers[cur][next] == 1) {
-                    visited[next] = true;
-                    q.offer(next);
-                }
-            }
+    public void dfs(int currNode, int n, int[][] computers, int[] visited) {
+        visited[currNode] = 1;
+        for(int i=0; i<n; i++) {
+            if(computers[currNode][i] == 1) {
+                if(visited[i] == 0) {
+                    visited[i] = 1;
+                    dfs(i, n, computers, visited);
+                }    
+            }    
         }
+        return;
     }
 }
