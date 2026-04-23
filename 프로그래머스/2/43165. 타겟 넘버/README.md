231e23a4a04ef5b637e57811a627581b379b95cc
JooeonLee <58818779+JooeonLee@users.noreply.github.com>
2026-04-23 19:48:39 +0900

    [level 2] Title: 타겟 넘버, Time: 15.71 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 75.2 MB, 시간: 4.46 ms
+메모리: 89.4 MB, 시간: 15.71 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 31일 12:13:50
+2026년 04월 23일 19:48:35
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,22 +1,34 @@
 class Solution {
-    int count = 0;
-    public int solution(int[] numbers, int target) {
-        dfs(numbers, 0, target, 0);
-        return count;
+    static int cnt = 0;
+    public static int solution(int[] numbers, int target) {
+        int[] visited = new int[numbers.length];
+        
+        dfs(0, 0, visited, numbers, target);
+        
+        return cnt;
     }
     
-    public void dfs(int[] numbers, int depth, int target, int result) {
-        if(depth == numbers.length) {
-            if(target == result) {
-                count++;
-            }
+    public static void dfs(int currResult, int currIdx, int[] visited, int[] numbers, int target) {
+        if(currResult == target && currIdx == visited.length) {
+            cnt++;
             return;
         }
-        
-        int plus = result + numbers[depth];
-        int minus = result - numbers[depth];
-        
-        dfs(numbers, depth+1, target, plus);
-        dfs(numbers, depth+1, target, minus);
+        else if(currIdx == visited.length) {
+            return;
+        } 
+        else {
+            for(int i=1; i<=2; i++) {
+                int flag=1;
+                if(i==1)
+                    flag=1;
+                else
+                    flag=-1;
+                
+                visited[currIdx] = 1;
+                int nextResult = currResult + numbers[currIdx] * flag;
+                dfs(nextResult, currIdx+1, visited, numbers, target);
+                visited[currIdx] = 0;   
+            }
+        }
     }
 }
