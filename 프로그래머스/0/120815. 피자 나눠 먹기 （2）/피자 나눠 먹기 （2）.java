dc2b82cfe7e6c6c8a4e036d1095bdabe92d842dc
JooeonLee <58818779+JooeonLee@users.noreply.github.com>
2026-04-28 09:21:28 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        
+        return n / gcd(n, 6);
+    }
+    
+    public int gcd(int a, int b) {
+        if(b == 0)
+            return a;
+        
+        return gcd(b, a%b);
+    }
+}
