696d87a3bcf5e95b63132b48d683e343250f3883
JooeonLee <58818779+JooeonLee@users.noreply.github.com>
2026-05-11 13:05:55 +0900

    [level 0] Title: 합성수 찾기, Time: 0.03 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int cnt = 0;
+        
+        for(int i=2; i<=n; i++)
+            if(isPrime(i))
+                cnt++;
+        
+        
+        return n-cnt-1;
+    }
+    
+    public boolean isPrime(int num) {
+        for(int i=2; i<=Math.sqrt(num); i++)
+            if(num % i == 0)
+                return false;
+        
+        return true;
+    }
+}
