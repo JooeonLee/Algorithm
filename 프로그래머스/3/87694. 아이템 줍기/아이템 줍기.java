15d2247f02413981c510a47193054956026ca414
JooeonLee <58818779+JooeonLee@users.noreply.github.com>
2026-05-20 09:06:39 +0900

    [level 3] Title: 아이템 줍기, Time: 0.91 ms, Memory: 61 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -1,20 +1,18 @@
 import java.util.*;
 
 class Solution {
-    static final int MAX = 102; // 좌표 최대 50 * 2 = 100, 여유 포함
+    static final int MAX = 102;
     static int[][] board = new int[MAX][MAX];
     static int[][] dist = new int[MAX][MAX];
     static final int[] dx = {1, -1, 0, 0};
     static final int[] dy = {0, 0, 1, -1};
     
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
-        // 1) 보드 초기화
         for (int i = 0; i < MAX; i++) {
             Arrays.fill(board[i], 0);
             Arrays.fill(dist[i], -1);
         }
 
-        // 2) 모든 직사각형 영역을 채운다(테두리+내부)
         for (int[] r : rectangle) {
             int x1 = r[0] * 2, y1 = r[1] * 2;
             int x2 = r[2] * 2, y2 = r[3] * 2;
@@ -25,7 +23,6 @@ class Solution {
             }
         }
 
-        // 3) 각 직사각형의 내부를 지워서 테두리만 남긴다
         for (int[] r : rectangle) {
             int x1 = r[0] * 2, y1 = r[1] * 2;
             int x2 = r[2] * 2, y2 = r[3] * 2;
@@ -36,7 +33,6 @@ class Solution {
             }
         }
 
-        // 4) BFS는 테두리(board==1)만 이동
         int sx = characterX * 2, sy = characterY * 2;
         int tx = itemX * 2, ty = itemY * 2;
 
@@ -55,7 +51,7 @@ class Solution {
                 int ny = y + dy[dir];
 
                 if (nx < 0 || ny < 0 || nx >= MAX || ny >= MAX) continue;
-                if (board[nx][ny] != 1) continue;      // 테두리만
+                if (board[nx][ny] != 1) continue;
                 if (dist[nx][ny] != -1) continue;
 
                 dist[nx][ny] = dist[x][y] + 1;
@@ -63,7 +59,6 @@ class Solution {
             }
         }
 
-        // 2배 좌표에서의 이동거리이므로 /2
         return dist[tx][ty] / 2;
     }
 }
