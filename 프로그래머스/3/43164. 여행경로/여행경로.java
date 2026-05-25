2a720417c393320db8ea11ada190f81f24f4e6ec
JooeonLee <58818779+JooeonLee@users.noreply.github.com>
2026-05-25 23:34:11 +0900

    [level 3] Title: 여행경로, Time: 0.64 ms, Memory: 61.2 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[][] tickets) {
-        // 출발지 -> (도착지들을 사전순으로 꺼내기 위한 PQ)
         Map<String, PriorityQueue<String>> graph = new HashMap<>();
         for (String[] t : tickets) {
             graph.computeIfAbsent(t[0], k -> new PriorityQueue<>()).offer(t[1]);
@@ -18,10 +17,8 @@ class Solution {
             PriorityQueue<String> pq = graph.get(cur);
 
             if (pq != null && !pq.isEmpty()) {
-                // 사전순으로 가장 앞선 목적지부터 사용
                 stack.push(pq.poll());
             } else {
-                // 더 이상 나갈 간선이 없으면 경로에 확정 (역순으로 쌓임)
                 route.add(stack.pop());
             }
         }
