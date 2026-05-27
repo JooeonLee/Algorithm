d2933377a89af9abe9a6382713aadbc07e0b2918
JooeonLee <58818779+JooeonLee@users.noreply.github.com>
2026-05-27 13:36:17 +0900

    [level 1] Title: 체육복, Time: 0.30 ms, Memory: 60.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,48 +2,40 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        // 정렬 (앞번호부터 처리하기 위함)
+        int answer = 0;
+        int[] check = new int[n+1];
+        Arrays.fill(check, 1);
+        
         Arrays.sort(lost);
         Arrays.sort(reserve);
-
-        // 체육복 상태 배열 (1-based index)
-        int[] clothes = new int[n + 1];
-        Arrays.fill(clothes, 1);
-
-        // 도난
-        for (int l : lost) {
-            clothes[l]--;
-        }
-
-        // 여벌
-        for (int r : reserve) {
-            clothes[r]++;
-        }
-
-        // 빌려주기 (그리디)
-        for (int i = 1; i <= n; i++) {
-            if (clothes[i] == 0) {
-                // 앞 학생에게 빌리기
-                if (i - 1 >= 1 && clothes[i - 1] == 2) {
-                    clothes[i - 1]--;
-                    clothes[i]++;
-                }
-                // 뒷 학생에게 빌리기
-                else if (i + 1 <= n && clothes[i + 1] == 2) {
-                    clothes[i + 1]--;
-                    clothes[i]++;
-                }
+        
+        for(int idx : lost)
+            check[idx]--;
+        
+        for(int idx : reserve)
+            check[idx]++;
+        
+        for(int i=0; i<lost.length; i++) {
+            int idx = lost[i];
+            
+            if(check[idx] >= 1)
+                continue;
+            
+            if(idx-1 >= 1 && check[idx-1] > 1) {
+                check[idx-1]--;
+                check[idx]++;
+            }
+            else if(idx+1 <= n && check[idx+1] > 1) {
+                check[idx+1]--;
+                check[idx]++;
             }
         }
-
-        // 체육수업 가능한 학생 수 계산
-        int answer = 0;
-        for (int i = 1; i <= n; i++) {
-            if (clothes[i] >= 1) {
+        
+        for(int c : check) {
+            if(c >= 1)
                 answer++;
-            }
         }
-
-        return answer;
+        
+        return answer-1;
     }
 }
