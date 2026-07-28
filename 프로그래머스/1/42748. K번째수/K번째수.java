ea7bf57f4a6f181823efddc4e74c4ca9138e4fa1
JooeonLee <58818779+JooeonLee@users.noreply.github.com>
2026-07-28 09:15:08 +0900

    [level 1] Title: K번째수, Time: 1.43 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,14 +1,14 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         
         for(int i=0; i<commands.length; i++) {
-            int[] currArr = new int[commands[i][1] - commands[i][0] + 1];
-            for(int j=0; j<commands[i][1]-commands[i][0]+1; j++)
-                currArr[j] = array[commands[i][0]-1+j];
-            Arrays.sort(currArr);
-            answer[i] = currArr[commands[i][2] - 1];
+            int[] command = commands[i];
+            int[] subArray = Arrays.copyOfRange(array, command[0]-1, command[1]);
+            Arrays.sort(subArray);
+            answer[i] = subArray[command[2]-1];
         }
         return answer;
     }
