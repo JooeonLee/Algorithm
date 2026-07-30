fbb89628949ba3e1798f519edf16812a6318bd7e
JooeonLee <58818779+JooeonLee@users.noreply.github.com>
2026-07-30 09:22:19 +0900

    [level 2] Title: H-Index, Time: 2.23 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,22 +2,20 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
-        int answer = 0;
+        ArrayList<Integer> citationList = new ArrayList<>();
+        for(int citation : citations)
+            citationList.add(citation);
+        citationList.sort(Comparator.reverseOrder());
         
-        int[] sortedArr = Arrays.stream(citations)
-            .boxed()
-            .sorted(Comparator.reverseOrder())
-            .mapToInt(Integer::intValue)
-            .toArray();
-        
-        int h = 0;
-        for(int i=0; i<sortedArr.length; i++) {
-            if(sortedArr[i] >= i+1)
-                h = i+1;
-            else
+        int hIdx = 0;
+        for(int i=0; i<citationList.size(); i++) {
+            if(citationList.get(i) < i+1)
                 break;
+            hIdx = i;
         }
         
-        return h;
+        if(hIdx == 0 && citationList.get(hIdx) == 0)
+            return 0;
+        return hIdx+1;
     }
 }
