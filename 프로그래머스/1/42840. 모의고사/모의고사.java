42415434e00e64fd6f04dc325c77a9472324e7d3
JooeonLee <58818779+JooeonLee@users.noreply.github.com>
2026-08-03 15:53:04 +0900

    [level 1] Title: 모의고사, Time: 4.01 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,29 +2,41 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] answer2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] answer3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int cnt1=0, cnt2=0, cnt3=0;
-        
-        for(int i=0; i<answers.length; i++) {
-            if(i%5+1 == answers[i])
-                cnt1++;
-            if(answer2[i%8] == answers[i])
-                cnt2++;
-            if(answer3[i%10] == answers[i])
-                cnt3++;
-        }
-        
-        int max = Math.max(cnt1, Math.max(cnt2, cnt3));
         ArrayList<Integer> answer = new ArrayList<>();
         
-        if(max == cnt1)
+        int cnt1 = 0;
+        int cnt2 = 0;
+        int cnt3 = 0;
+        
+        for(int i=1; i<=answers.length; i++) {
+            int curr = answers[i-1];
+            int case1 = i%5==0 ? 5 : i%5;
+            if(curr == case1)
+                cnt1++;
+            
+            int case2Idx = (i%8==0 ? 8 : i%8) -1;
+            int[] case2Arr = {2,1,2,3,2,4,2,5};
+            int case2 = case2Arr[case2Idx];
+            if(curr == case2)
+                cnt2++;
+            
+            int case3Idx = (i%10==0 ? 10 : i%10) - 1;
+            int[] case3Arr = {3,3,1,1,2,2,4,4,5,5};
+            int case3 = case3Arr[case3Idx];
+            if(curr == case3)
+                cnt3++;
+        }
+        int maxCnt = Math.max(cnt1, Math.max(cnt2, cnt3));
+        
+        if(cnt1 == maxCnt)
             answer.add(1);
-        if(max == cnt2)
+        if(cnt2 == maxCnt)
             answer.add(2);
-        if(max == cnt3)
+        if(cnt3 == maxCnt)
             answer.add(3);
+        
         return answer.stream()
+            .sorted()
             .mapToInt(Integer::intValue)
             .toArray();
     }
