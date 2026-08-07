b7adf9b60f2301235f8a52f815cdb1eca4392593
JooeonLee <58818779+JooeonLee@users.noreply.github.com>
2026-08-07 09:07:26 +0900

    [level 2] Title: 피로도, Time: 2.98 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,32 +1,29 @@
 import java.util.*;
-import java.util.*;
 
 class Solution {
-    private int max = 0;
-    private boolean[] visited;
+    int max = 0;
+    boolean[] visited;
     
     public int solution(int k, int[][] dungeons) {
+        int answer = -1;
         visited = new boolean[dungeons.length];
-        
         dfs(0, k, dungeons);
-        
         return max;
     }
     
-    public void dfs(int cnt, int k, int[][] dungeons) {
+    private void dfs(int cnt, int k, int[][] dungeons) {
         max = Math.max(max, cnt);
         
         for(int i=0; i<dungeons.length; i++) {
             int need = dungeons[i][0];
             int cost = dungeons[i][1];
             
-            if(k >= need && !visited[i]) {
+            if(!visited[i] && k >= need) {
                 visited[i] = true;
-                
                 dfs(cnt+1, k-cost, dungeons);
-                
                 visited[i] = false;
             }
         }
     }
 }
+
