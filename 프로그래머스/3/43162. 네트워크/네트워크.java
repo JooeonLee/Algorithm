c5c7998385ef70897802d5b5ddf358ad7e8e95ba
JooeonLee <58818779+JooeonLee@users.noreply.github.com>
2026-08-10 13:40:19 +0900

    [level 3] Title: 네트워크, Time: 1.25 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,34 +1,43 @@
+import java.util.*;
+
 class Solution {
-    int n;
-    int[][] computers;
     boolean[] visited;
-    
     public int solution(int n, int[][] computers) {
-        this.n = n;
-        this.computers = computers;
-        this.visited = new boolean[n];
         
-        int answer = 0;
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        visited = new boolean[n+1];
+        
+        for(int i=0; i<=computers.length; i++) {
+            graph.add(new ArrayList<Integer>());
+        }
         
         for(int i=0; i<n; i++) {
-            if(this.visited[i] == false) {
-                answer++;
-                dfs(i);
+            for(int j=0; j<n; j++) {
+                if(computers[i][j] == 1) {
+                    graph.get(i+1).add(j+1);
+                    graph.get(j+1).add(i+1);
+                }
             }
         }
         
-        return answer;
+        
+        int cnt = 0;
+        for(int i=1; i<=n; i++) {
+            if(!visited[i]) {
+                cnt++;
+                dfs(i, graph);
+            }
+        }
+        
+        return cnt;
     }
     
-    public void dfs(int node) {
-        if(this.visited[node] == true)
-            return;
+    private void dfs(int node, ArrayList<ArrayList<Integer>> graph) {
+        visited[node] = true;
         
-        this.visited[node] = true;
-        
-        for(int i=0; i<this.n; i++) {
-            if(computers[node][i] == 1)
-                dfs(i);
+        for(int nextNode : graph.get(node)) {
+            if(!visited[nextNode])
+                dfs(nextNode, graph);
         }
     }
 }
