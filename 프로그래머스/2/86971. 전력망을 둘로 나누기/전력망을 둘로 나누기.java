662302e9471726fa3449aad99f8688ab9a861dfe
JooeonLee <58818779+JooeonLee@users.noreply.github.com>
2026-08-13 13:32:40 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.75 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,46 +1,57 @@
 import java.util.*;
 
 class Solution {
+    boolean[] visited;
     public int solution(int n, int[][] wires) {
         int answer = n;
+        visited = new boolean[n+1];
+        
+        ArrayList<ArrayList<Integer>> tree = new ArrayList<>();
+        for(int i=0; i<n+1; i++)
+            tree.add(new ArrayList<>());
         
         for(int i=0; i<n-1; i++) {
-            ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-            for(int j=0; j<=n; j++)
-                graph.add(new ArrayList<>());
-            
-            for(int j=0; j<n-1; j++) {
-                if(i == j)
-                    continue;
-                int from = wires[j][0];
-                int to = wires[j][1];
-                
-                graph.get(from).add(to);
-                graph.get(to).add(from);
-            }
-            
-            boolean[] visited = new boolean[n+1];
-            
-            int currCnt = dfs(1, visited, graph);
-            int otherCnt = n - currCnt;
-            int currDiff = Math.abs(currCnt - otherCnt);
-            
-            answer = answer > currDiff ? currDiff : answer;
+            int[] edge = wires[i];
+            tree.get(edge[0]).add(edge[1]);
+            tree.get(edge[1]).add(edge[0]);
         }
         
+        for(int i=0; i<n-1; i++) {
+            int[] removeEdge = wires[i];
+            tree.get(removeEdge[0]).remove(Integer.valueOf(removeEdge[1]));
+            tree.get(removeEdge[1]).remove(Integer.valueOf(removeEdge[0]));
+            
+            Arrays.fill(visited, false);
+            dfs(removeEdge[0], tree);
+            int comp1 = 0;
+            int comp2 = 0;
+            int currDiff = 0;
+            
+            for(int j=1; j<n+1; j++)
+                if(visited[j])
+                    comp1++;
+            comp2 = n - comp1;
+            currDiff = Math.abs(comp1 - comp2);
+            answer = Math.min(answer, currDiff);
+            
+            tree.get(removeEdge[0]).add(removeEdge[1]);
+            tree.get(removeEdge[1]).add(removeEdge[0]);
+        }
         return answer;
     }
     
-    public int dfs(int currNode, boolean[] visited, ArrayList<ArrayList<Integer>> graph) {
-        int cnt = 1;
-        visited[currNode] = true;
-        ArrayList<Integer> curr = graph.get(currNode);
+    void dfs(int idx, ArrayList<ArrayList<Integer>> tree) {
+        visited[idx] = true;
         
-        for(int next : curr) {
-            if(!visited[next]) {
-                cnt += dfs(next, visited, graph);
-            }
+        ArrayList<Integer> connected = tree.get(idx);
+        for(int nextIdx : connected) {
+            if(!visited[nextIdx])
+                dfs(nextIdx, tree);
         }
-        return cnt;
     }
 }
+
+/**
+2 <= n <= 100
+입력이 반드시 트리로 주어진다
+*/
