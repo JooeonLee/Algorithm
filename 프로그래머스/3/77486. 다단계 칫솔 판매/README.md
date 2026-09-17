78cde7001cb92432fa35395f97a41bda35be2f0e
JooeonLee <58818779+JooeonLee@users.noreply.github.com>
2026-09-17 09:36:01 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 10.09 ms, Memory: 151 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/README.md
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/README.md
@@ -1,10 +1,10 @@
 # [level 3] 다단계 칫솔 판매 - 77486 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77486?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77486) 
 
 ### 성능 요약
 
-메모리: 140 MB, 시간: 10.05 ms
+메모리: 151 MB, 시간: 10.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 09월 17일 06:56:45
+2026년 09월 17일 09:35:57
 
 ### 문제 설명
 
--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -1,54 +1,32 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(String[] enroll, String[] referral,
-                          String[] seller, int[] amount) {
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        Map<String, Integer> index = new HashMap<>();
+        int[] parents = new int[enroll.length];
+        int[] answer = new int[enroll.length];
         
-        int n = enroll.length;
-        
-        // 이름 -> 번호
-        HashMap<String, Integer> indexMap = new HashMap<>();
-        
-        for (int i = 0; i < n; i++) {
-            indexMap.put(enroll[i], i);
+        for(int i=0; i<enroll.length; i++) {
+            index.put(enroll[i], i);
         }
-        
-        // parent[i] = i번 판매원의 추천인
-        // 추천인이 center라면 -1
-        int[] parent = new int[n];
-        
-        for (int i = 0; i < n; i++) {
-            if (referral[i].equals("-")) {
-                parent[i] = -1;
-            } else {
-                parent[i] = indexMap.get(referral[i]);
-            }
+        for(int i=0; i<enroll.length; i++) {
+            parents[i] = index.getOrDefault(referral[i], -1);
         }
-        
-        int[] answer = new int[n];
-        
-        // 각각의 판매 기록 처리
-        for (int i = 0; i < seller.length; i++) {
-            int curr = indexMap.get(seller[i]);
-            int money = amount[i] * 100;
+        for(int i=0; i<seller.length; i++) {
+            String curr = seller[i];
+            int currIdx = index.get(curr);
+            int currPrice = amount[i] * 100;
+            answer[currIdx] += currPrice - currPrice/10;
             
-            // 판매자부터 추천인을 따라 위로 올라간다.
-            while (curr != -1 && money > 0) {
-                int give = money / 10;
-                int keep = money - give;
-                
-                answer[curr] += keep;
-                
-                // 더 이상 추천인에게 전달할 돈이 없다.
-                if (give == 0) {
-                    break;
-                }
-                
-                money = give;
-                curr = parent[curr];
+            int parentIdx = parents[currIdx];
+            
+            while(currPrice / 10 > 0 && parentIdx != -1) {
+                answer[parentIdx] += currPrice/10 - currPrice/100;
+                currPrice = currPrice / 10;
+                currIdx = parentIdx;
+                parentIdx = parents[currIdx];
             }
         }
-        
         return answer;
     }
 }
