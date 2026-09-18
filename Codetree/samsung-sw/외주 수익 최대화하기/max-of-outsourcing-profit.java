72aff66e26fb77451317fa3d7af7f3175f111101
JooeonLee <58818779+JooeonLee@users.noreply.github.com>
2026-09-18 11:05:30 +0900

    [Passed] 외주 수익 최대화하기 (242ms, 14MB)

--- Codetree/samsung-sw/외주 수익 최대화하기/max-of-outsourcing-profit.java
+++ Codetree/samsung-sw/외주 수익 최대화하기/max-of-outsourcing-profit.java
@@ -2,63 +2,48 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static class Work {
-        int start;
-        int end;
-        int income;
+    static class Task {
+        int s;
+        int e;
+        int c;
 
-        public Work(int start, int end, int income) {
-            this.start = start;
-            this.end = end;
-            this.income = income;
+        public Task(int s, int e, int c) {
+            this.s = s;
+            this.e = e;
+            this.c = c;
         }
     }
     public static void main(String[] args) {
         // Please write your code here.
         FastReader fr = new FastReader();
-
-        int n = fr.nextInt();
-        Work[] works = new Work[n+1];
-
-        for(int i=1; i<=n; i++) {
-            int duration = fr.nextInt();
-            int income = fr.nextInt();
-            int end = i + duration - 1;
-
-            works[i] = new Work(i, end, income);
+        int N = fr.nextInt();
+        int[] dp = new int[N+1];
+        ArrayList<Task> tasks = new ArrayList<>();
+        for(int i=1; i<=N; i++) {
+            int p = fr.nextInt();
+            int c = fr.nextInt();
+            Task currTask = new Task(i, i+p-1, c);
+            if(i+p-1 <= N)
+                tasks.add(currTask);
         }
+        tasks.sort((a, b) -> {
+            if(a.e != b.e)
+                return Integer.compare(a.e, b.e);
+            return Integer.compare(b.c, a.c); 
+        });
 
-        // 수정함 처음에 -1이었음
-        int maxIncome = 0;
-
-        Queue<int[]> queue = new ArrayDeque<>();
-        for(int i=1; i<=n; i++) {
-            if(works[i].end > n)
-                continue;
-            queue.offer(new int[] {works[i].start, works[i].income});
-            maxIncome = Math.max(maxIncome, works[i].income);
-        }
-        while(!queue.isEmpty()) {
-            int[] curr = queue.poll();
-            int currStart = curr[0];
-            int currIncome = curr[1];
-            Work currWork = works[currStart];
-            int nextPossibleStart = currWork.end + 1;
-            if(nextPossibleStart > n)
-                continue;
-
-            for(int i=nextPossibleStart; i<=n; i++) {
-                int nextIncome = currIncome + works[i].income;
-                int nextEnd = works[i].end;
-                if(nextEnd > n)
-                    continue;
-                maxIncome = Math.max(maxIncome, nextIncome);
-
-                queue.offer(new int[]{i, nextIncome});
+        dp[0] = 0;
+        int idx = 0;
+        for(int day=1; day<=N; day++) {
+            dp[day] = Math.max(dp[day], dp[day-1]);
+            
+            while(idx < tasks.size() && tasks.get(idx).e == day) {
+                Task curr = tasks.get(idx);
+                dp[day] = Math.max(dp[day], dp[curr.s-1] + curr.c);
+                idx++;
             }
         }
-
-        System.out.println(maxIncome);
+        System.out.println(dp[N]);
     }
 
     static class FastReader {
@@ -70,7 +55,7 @@ public class Main {
         }
 
         String next() {
-            while(st == null || !st.hasMoreTokens()) {
+            while(st==null || !st.hasMoreTokens()) {
                 try {
                     st = new StringTokenizer(br.readLine());
                 } catch(Exception e) {
