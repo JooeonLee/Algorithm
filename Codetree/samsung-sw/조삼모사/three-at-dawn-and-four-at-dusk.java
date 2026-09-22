c410a2e9e150889fc28773ab9e831ece01b30a8e
JooeonLee <58818779+JooeonLee@users.noreply.github.com>
2026-09-22 17:06:17 +0900

    [Passed] 조삼모사 (288ms, 11MB)

--- Codetree/samsung-sw/조삼모사/three-at-dawn-and-four-at-dusk.java
+++ Codetree/samsung-sw/조삼모사/three-at-dawn-and-four-at-dusk.java
@@ -2,72 +2,69 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+
     static int[][] map;
     static boolean[] visited;
     static int n;
     static int minDiff = Integer.MAX_VALUE;
 
     public static void main(String[] args) {
-        // Please write your code here.
+
         FastReader fr = new FastReader();
+
         n = fr.nextInt();
+
         map = new int[n][n];
         visited = new boolean[n];
 
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
                 map[i][j] = fr.nextInt();
             }
         }
 
         dfs(0, 0);
 
-        System.out.println(minDiff/2);
+        System.out.println(minDiff);
     }
 
     static void dfs(int currIdx, int selectedCnt) {
-        if(selectedCnt == n/2) {
+
+        if(selectedCnt == n / 2) {
             calculate();
             return;
         }
-        
-        for(int i=currIdx+1; i<n; i++) {
-            if(visited[i])
-                continue;
+
+        for(int i = currIdx + 1; i < n; i++) {
             visited[i] = true;
-            dfs(i, selectedCnt+1);
+            dfs(i, selectedCnt + 1);
             visited[i] = false;
         }
     }
 
     static void calculate() {
+
         int morning = 0;
         int evening = 0;
 
-        for(int i=0; i<n; i++) {
-            if(visited[i]) {
-                for(int j=0; j<n; j++) {
-                    if(visited[j]) {
-                        morning += map[i][j];
-                        morning += map[j][i];
-                    }
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+
+                if(visited[i] && visited[j]) {
+                    morning += map[i][j] + map[j][i];
                 }
-            }
-            else {
-                for(int j=0; j<n; j++) {
-                    if(!visited[j]) {
-                        evening += map[i][j];
-                        evening += map[j][i];
-                    }
+                else if(!visited[i] && !visited[j]) {
+                    evening += map[i][j] + map[j][i];
                 }
             }
         }
-        
-        int diff = Math.abs(morning-evening);
+
+        int diff = Math.abs(morning - evening);
         minDiff = Math.min(minDiff, diff);
     }
 
     static class FastReader {
+
         BufferedReader br;
         StringTokenizer st;
 
